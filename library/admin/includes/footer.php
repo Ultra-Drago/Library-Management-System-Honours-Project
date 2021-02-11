c2d3ee74748999c906a268006aafea97fe0ce92f
Ultra-Drago <72343592+Ultra-Drago@users.noreply.github.com>
2021-02-12 00:04:54 +0530

    Add files via upload

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,20 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>V</strong><span class = "small footerSubtext">ishwakarma</span>
+                        <strong>I</strong><span class = "small footerSubtext">nstitute of</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        666, Upper Indira Nagar, Bibwewadi, Pune, Maharashtra 411037
+                        &copy; Event Management Systems, 2021.
+                    </p>
+                </div>
+        </div>
+    </div>
+</footer>
